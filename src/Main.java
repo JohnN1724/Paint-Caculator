e442ec7dfa67ebaadd2c3235bffd1fe32a257f66
Johnn <john.nuttall17@gmail.com>
2022-08-04 10:23:26 +0100

    Added wall area calculation class

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,11 @@ public class Main {
         System.out.println("Enter height of wall in metres:");
         float wallHeight = height.nextLong();  // Read user input
 
-        float canvasArea = wallWidth*wallHeight; // Gets area of wall
-
-        float oneCoat = canvasArea/12; // 1 Litre of paint on average covers 12 metres squared of wall
-
-        System.out.println("You will need "+ oneCoat + " litres to paint wall with one coat");
-
         Scanner coats = new Scanner(System.in);  // Create a Scanner object for number of coats
         System.out.println("How many coats would you like to apply?");
         int coatNo = coats.nextInt();  // Read user input
 
-        float paintNeeded = oneCoat * coatNo;
 
-        System.out.println("You will need "+ paintNeeded + " litres to paint wall overall");
 
     }
 }
