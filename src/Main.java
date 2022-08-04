7637bf388e47d8da3dc31b7e019496f772c86825
Johnn <john.nuttall17@gmail.com>
2022-08-04 16:23:28 +0100

    Create method for creating an object for wall

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,43 @@ import java.util.*;
 public class Main {
     public static void main(String args[]){
 
-        Scanner width = new Scanner(System.in);  // Create a Scanner object for width
-        System.out.println("Enter width of wall in metres:");
-        float wallWidth = width.nextLong();  // Read user input
+        LinkedList<Wall> walls = new LinkedList<Wall>();
 
-        Scanner height = new Scanner(System.in);  // Create a Scanner object for height
+        Wall w = new Wall();
+
+        Scanner Width = new Scanner(System.in);  // Create a Scanner object for width
+        System.out.println("Enter width of wall in metres:");
+        float width = Width.nextLong();  // Read user input
+        w.setWallWidth(width);
+
+        Scanner Height = new Scanner(System.in);  // Create a Scanner object for height
         System.out.println("Enter height of wall in metres:");
-        float wallHeight = height.nextLong();  // Read user input
+        float height = Height.nextLong();  // Read user input
+        w.setWallHeight(height);
 
         Scanner coats = new Scanner(System.in);  // Create a Scanner object for number of coats
         System.out.println("How many coats would you like to apply?");
         int coatNo = coats.nextInt();  // Read user input
+        w.setNoCoats(coatNo);
 
+        Scanner type = new Scanner(System.in);  // Create a Scanner object for number of coats
+        System.out.println("What paint type would you like to use?");
+        String paintType = type.nextLine();  // Read user input
+        w.setPaintType(paintType);
 
+        Scanner shape = new Scanner(System.in);  // Create a Scanner object for number of coats
+        System.out.println("Is the wall 4 sided or 3 sided?");
+        int wallSides = shape.nextInt();  // Read user input
 
+        if(wallSides == 3){
+            w.setWallArea(w.calcTriangle(width, height));
+        } else if(wallSides == 4){
+            w.setWallArea(w.calcQuadrilateral(width, height));
+        }
 
+        walls.add(w);
 
+        System.out.println(w.toString());
+        System.out.println(walls);
     }
 }
---
+++ src/Wall.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Wall {
+
+    private int wallNo;
+    private float wallWidth;
+    private float wallHeight;
+    private int NoCoats;
+    private float wallArea;
+    private String paintType;
+
+    public Wall() {
+        this.wallNo = wallNo;
+        this.wallWidth = wallWidth;
+        this.wallHeight = wallHeight;
+        this.NoCoats = NoCoats;
+        this.wallArea = wallArea;
+        this.paintType = paintType;
+    }
+
+    public int getWallNo() {
+        return wallNo;
+    }
+
+    public float getWallWidth() {
+        return wallWidth;
+    }
+    public float getWallHeight() {
+        return wallHeight;
+    }
+    public int getNoCoats() {
+        return NoCoats;
+    }
+    public float getWallArea() {
+        return wallArea;
+    }
+    public String getPaintType() {
+        return paintType;
+    }
+
+    public void setWallNo(int wallNo) {
+        this.wallNo = wallNo;
+    }
+    public void setNoCoats(int noCoats) {
+        NoCoats = noCoats;
+    }
+    public void setWallWidth(float wallWidth) {
+        this.wallWidth = wallWidth;
+    }
+    public void setWallHeight(float wallHeight) {
+        this.wallHeight = wallHeight;
+    }
+    public void setWallArea(float wallArea) {
+        this.wallArea = wallArea;
+    }
+    public void setPaintType(String paintType) {
+        this.paintType = paintType;
+    }
+
+    @Override
+    public String toString() {
+        return "Wall{" +
+                ", wallWidth=" + wallWidth +
+                ", wallHeight=" + wallHeight +
+                ", NoCoats=" + NoCoats +
+                ", wallArea=" + wallArea +
+                ", paintType='" + paintType + '\'' +
+                '}';
+    }
+
+    public float calcQuadrilateral(float wallWidth, float wallHeight){
+        float wallArea = wallWidth * wallHeight;
+
+        return wallArea;
+    }
+
+    public float calcTriangle(float wallWidth, float wallHeight){
+        float wallArea = (wallWidth * wallHeight)/2;
+
+        return wallArea;
+    }
+
+    public float calcPaintNeeded(float wallArea, int noCoats){
+        float paintNeeded = wallArea * noCoats;
+
+        return paintNeeded;
+    }
+
+
+}
